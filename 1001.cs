325b7dcb7dee37d1a92f42e34a26e11609d4c02f
신호준 <tlsghwns3366@naver.com>
2021-05-19 02:19:02 +0900

    1001

---
+++ 1001.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            string[] b;
+            a = Console.ReadLine();
+            b = a.Split(' ');
+            int c;
+            c = Convert.ToInt32(b[0]) - Convert.ToInt32(b[1]);
+            Console.Write(c);
+        }
+    }
+}
