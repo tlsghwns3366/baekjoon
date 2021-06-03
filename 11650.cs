92e8f6325e0ea4751358c4dde574d0008ba027bd
신호준 <tlsghwns3366@naver.com>
2021-06-03 20:07:01 +0900

    11650

---
+++ 11650.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            int[][] b = new int[a][];
+            for (int i = 0; i < a; i++)
+            {
+                string[] c = Console.ReadLine().Split(' ');
+                b[i] = new int[] { int.Parse(c[0]), int.Parse(c[1]) };
+            }
+            var sort = b.OrderBy(x => x[0]).ThenBy(x => x[1]);
+            foreach (int[] e in sort)
+            {
+                sb.AppendLine(e[0] + " " + e[1]);
+            }
+            Console.Write(sb);
+        }
+    }
+}
