d9c8a2962c98bb023d7ee5c4bcea0b404bfdb11c
신호준 <tlsghwns3366@naver.com>
2021-07-27 20:54:55 +0900

    15651

---
+++ C++/15651/15651.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+void back(int *nm, int n, int m, int z)
+{
+    if (z == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            printf("%d ", nm[i]);
+        }
+        printf("\n");
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+            nm[z] = i + 1;
+            back(nm, n, m, z + 1);
+    }
+}
+int main()
+{
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    int *nm = new int[m];
+
+    for (int i = 0; i < m; i++)
+    {
+        nm[i] = 0;
+    }
+
+    back(nm, n, m, 0);
+}
