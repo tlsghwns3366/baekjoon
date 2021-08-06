4aaf8148cfb955959b58c5e5bed71a25bf001459
신호준 <tlsghwns3366@naver.com>
2021-08-06 20:39:49 +0900

    11054

--- C++/11054/11054.cpp
+++ C++/11054/11054.cpp
@@ -4,9 +4,9 @@ int main()
 {
 	int n;
 	cin >> n;
-	int* a = new int[n] {0};
-	int* b = new int[n] {0};
-	int* c = new int[n] {0};
+	int *a = new int[n]{0};
+	int *b = new int[n]{0};
+	int *c = new int[n]{0};
 
 	for (int i = 0; i < n; i++)
 	{
@@ -22,14 +22,14 @@ int main()
 			if (a[j] < a[i])
 			{
 				if (maxone <= b[j])
-					maxone = b[i]+b[j];
+					maxone = b[i] + b[j];
 			}
 		}
-		c[n-i-1]++;
+		c[n - i - 1]++;
 		int maxtwo = 0;
-		for (int j =n- i-1; j < n; j++)
+		for (int j = n - i - 1; j < n; j++)
 		{
-			if (a[n-i-1] > a[j])
+			if (a[n - i - 1] > a[j])
 			{
 				if (maxtwo <= c[j])
 					maxtwo = c[n - i - 1] + c[j];
@@ -43,5 +43,5 @@ int main()
 		if (b[i] + c[i] > maxsum)
 			maxsum = b[i] + c[i];
 	}
-	cout << maxsum+1;
+	cout << maxsum + 1;
 }
