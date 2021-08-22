9c323f84829ef9ec1c0a7268712ff99e907e6f26
신호준 <tlsghwns3366@naver.com>
2021-08-23 01:51:14 +0900

    17298

--- C++/17298/17298.cpp
+++ C++/17298/17298.cpp
@@ -6,43 +6,24 @@ int main()
 {
     int n;
     cin >> n;
-    int *a = new int[n]{0};
-    int *b = new int[n]{0};
+    int *a = new int[n]{};
+    int *b = new int[n]{};
+    fill_n(b, n, -1);
+
     stack<int> s;
     stack<int> p;
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] < a[i + 1])
+        while (!s.empty() && s.top() < a[i])
         {
-            b[i] = a[i + 1];
-            while (!s.empty() && s.top() < a[i + 1])
-            {
-                b[p.top()] = a[i + 1];
-                s.pop();
-                p.pop();
-            }
+            b[p.top()] = a[i];
+            s.pop();
+            p.pop();
         }
-        else
-        {
-            s.push(a[i]);
-            p.push(i);
-        }
+        s.push(a[i]);
+        p.push(i);
     }
-    s.pop();
-    p.pop();
-    while (!s.empty())
-    {
-        b[p.top()] = -1;
-
-        s.pop();
-        p.pop();
-    }
-    b[n - 1] = -1;
     for (int i = 0; i < n; i++)
     {
         cout << b[i] << " ";
