690b7b64471385d685706ff95da2d197e12d8d5f
신호준 <tlsghwns3366@naver.com>
2021-10-03 22:00:05 +0900

    11279

---
+++ C++/11279/11279.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int nn[100002];
+    fill(nn, nn + 100002, 1);
+    int size = 1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nn[size];
+        if (nn[size] == 0)
+        {
+            cout << nn[1] << "\n";
+            nn[1] = nn[size - 1];
+            nn[size - 1] = 0;
+            if (size > 1)
+                size--;
+            int a = 1;
+            int b = a * 2;
+
+            if (nn[b] < nn[b + 1])
+                b = b + 1;
+            while (b <= size && nn[b] > nn[a])
+            {
+                int temp = nn[b];
+                nn[b] = nn[a];
+                nn[a] = temp;
+                a = b;
+                b = a * 2;
+                if (nn[b] < nn[b + 1])
+                    b = b + 1;
+            }
+        }
+        else
+        {
+            int a = size;
+            int b = size / 2;
+            while (a > 1 && nn[b] < nn[a])
+            {
+                int temp = nn[b];
+                nn[b] = nn[a];
+                nn[a] = temp;
+                a = b;
+                b = a / 2;
+            }
+            size++;
+        }
+    }
+}
