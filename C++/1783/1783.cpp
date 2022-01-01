6d92da0ce099afa95477aeae3882387d3c7cc6fe
신호준 <tlsghwns3366@naver.com>
2022-01-01 20:27:12 +0900

    1783

---
+++ C++/1783/1783.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    if (n == 1)
+    {
+        cout << 1;
+    }
+    else if (n == 2)
+    {
+        cout << min(4, (m + 1) / 2);
+    }
+    else if (n >= 3)
+    {
+        if (m <= 6)
+            cout << min(4, m);
+        else
+            cout << m - 2;
+    }
+}
