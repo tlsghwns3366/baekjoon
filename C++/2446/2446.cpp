04b6e25316a108f6481fb02ca521e61ddf477d85
신호준 <tlsghwns3366@naver.com>
2022-02-14 22:43:18 +0900

    2446

---
+++ C++/2446/2446.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int n = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * (n - i) - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 2; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * (i + 2) - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
