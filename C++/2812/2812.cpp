1efd81402ad9025cd4f90bf9d9c80178ef060fd5
신호준 <tlsghwns3366@naver.com>
2022-04-11 20:00:36 +0900

    git push origin master

---
+++ C++/2812/2812.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    stack<char> ansstack;
+    for (int i = 0; i < s.length(); i++)
+    {
+        while (k && !ansstack.empty() && ansstack.top() < s[i])
+        {
+            ansstack.pop();
+            k--;
+        }
+        ansstack.push(s[i]);
+    }
+    stack<char> outans;
+    while (!ansstack.empty())
+    {
+        outans.push(ansstack.top());
+        ansstack.pop();
+    }
+    int temp = outans.size();
+    for (int i = 0; i < temp-k; i++)
+    {
+        cout << outans.top();
+        outans.pop();
+    }
+}
