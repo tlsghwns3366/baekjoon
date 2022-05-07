dbfc2d21b3412aed377b054582310dd56615a470
신호준 <tlsghwns3366@naver.com>
2022-05-07 15:44:00 +0900

    4889

---
+++ C++/4889/4889.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int c = 0;
+    while (true)
+    {
+        string s;
+        cin >> s;
+        if (s[0] == '-')
+            break;
+        int ans = 0;
+        c++;
+        while (true)
+        {
+            if (s.find("{}") == s.find("{{") == s.find("}}") == s.find("}{") == string::npos || s == "")
+                break;
+            if (s.find("{}") != string::npos)
+                s.erase(s.find("{}"), 2);
+            else if (s.find("{{") != string::npos)
+            {
+                ans += 1;
+                s.erase(s.find("{{"), 2);
+            }
+            else if (s.find("}}") != string::npos)
+            {
+                ans += 1;
+                s.erase(s.find("}}"), 2);
+            }
+            else if (s.find("}{") != string::npos)
+            {
+                ans += 2;
+                s.erase(s.find("}{"), 2);
+            }
+        }
+        cout << c << ". " << ans << "\n";
+    }
+}
