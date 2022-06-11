376fb3c8d742e6241491cfd0a9a5a0aafc98de66
신호준 <tlsghwns3366@naver.com>
2022-06-11 20:27:13 +0900

    13699

---
+++ C++/13699/13699.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    long long dp[n + 1] = {
+        0,
+    };
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            dp[i] += dp[j] * dp[i - j - 1];
+        }
+    }
+    cout << dp[n];
+}
