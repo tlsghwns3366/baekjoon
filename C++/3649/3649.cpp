ef5eefb58b42e3d922cc0901edf303a54fab95c1
신호준 <tlsghwns3366@naver.com>
2022-07-26 13:18:43 +0900

    3273

---
+++ C++/3649/3649.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int x, n;
+int e[1000001];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (cin >> x)
+    {
+        x *= 10000000;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> e[i];
+        }
+        sort(e, e + n);
+        int a = 0;
+        int b = n - 1;
+        int sum = 0;
+        while (a < b)
+        {
+            sum = e[a] + e[b];
+            if (sum == x)
+                break;
+            else if (sum < x)
+                a++;
+            else
+                b--;
+        }
+        if (sum == x)
+            cout << "yes " << e[a] << " " << e[b] << endl;
+        else
+            cout << "danger\n";
+    }
+    return 0;
+}
