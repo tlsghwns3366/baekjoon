a92d7580c25ce1ebc36923cfff0ecc9672b29d59
신호준 <tlsghwns3366@naver.com>
2022-07-26 14:08:34 +0900

    11004

---
+++ C++/11004/11004.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> v;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end());
+    cout << v[k - 1];
+}
