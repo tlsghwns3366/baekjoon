d4c597d69e1ae7bb559ca7b113e4f49c0ceb698d
신호준 <tlsghwns3366@naver.com>
2023-01-25 23:52:37 +0900

    19939

---
+++ C++/19939/19939.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    int nn[1001];
+    for (int i = 1; i <= k; i++)
+    {
+        ans += i;
+    }
+    n -= ans;
+    if (n < 0)
+    {
+        cout<<"-1";
+    }
+    else
+    {
+        nn[0] = n / k;
+        n -= (n / k) * k;
+        for (int i = 1; i <= k; i++)
+        {
+            nn[i] = nn[i - 1] + 1;
+        }
+        for (int i = k; n > 0; i--)
+        {
+            n--;
+            nn[i]++;
+        }
+        cout << nn[k] - nn[1];
+    }
+}
