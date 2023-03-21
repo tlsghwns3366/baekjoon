92bf2f5822a9d75052a5923e39671807644cb217
신호준 <tlsghwns3366@naver.com>
2023-03-21 21:31:52 +0900

    6588

--- C++/6588/6588.cpp
+++ C++/6588/6588.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 using namespace std;
+bool dp[1000000];
 bool f(int a)
 {
     for (int i = 2; i * i <= a; i++)
     {
         if (a % i == 0)
-            return false;
+            return dp[a] = false;
     }
-    return true;
+    return dp[a] = true;
 }
 int main()
 {
     cin.tie(NULL);
+    for (int i = 3; i <= 1000000; i += 2)
+    {
+        f(i);
+    }
     while (true)
     {
         int n;
@@ -20,7 +25,7 @@ int main()
             break;
         for (int i = 3; i <= 1000000; i += 2)
         {
-            if (f(i) && f(n - i))
+            if (dp[i] && dp[n - i])
             {
                 cout << n << " = " << i << " + " << n - i << "\n";
                 break;
