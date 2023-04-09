2a1b389914b39124329e8858237b8beefeccc771
신호준 <tlsghwns3366@naver.com>
2023-04-10 01:01:51 +0900

    1402

---
+++ C++/1402/1402.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int q = 0; q < t; q++)
+    {
+        int sum = 0;
+        int a, b;
+        cin >> a >> b;
+        cout<<"yes\n";
+    }
+}
